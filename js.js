6e7526f45ffb61a8690af616367f0320c20bbd21
Xenacode-art <kajaobinna1@gmail.com>
2020-12-06 12:19:03 -0500

    my second web blog

---
+++ js.js
@@ -0,0 +1,4 @@
+alert ("WELCOME TO XENA'S CREATIVITY");
+alert ("Enjoy my work and draw inspiration from it");
+alert ("Click on words for its hovering effect to be activated..");
+alert ("Dont forget to upvote!!");
